ef7d9a43fa89c9cc43b95cb65251e55b917cd469
Gilang <wisnusumirat1@gmail.com>
2026-06-26 09:55:55 +0700

    UAS PBO Gilang

--- db_uas_pbo_gilangrinakitwisnuadhityasumirat.sql
+++ db_uas_pbo_gilangrinakitwisnuadhityasumirat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 26, 2026 at 02:50 AM
+-- Generation Time: Jun 26, 2026 at 02:55 AM
 -- Server version: 8.4.3
 -- PHP Version: 8.3.30
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_uas_pbo_trpl_gilangrinakitwisnuadhityasumirat`
+-- Database: `db_uas_pbo_gilangrinakitwisnuadhityasumirat`
 --
 
 -- --------------------------------------------------------
@@ -58,22 +58,7 @@ INSERT INTO `tabel_mahasiswa` (`id_mahasiswa`, `nama_mahasiswa`, `nim`, `semeste
 (9, 'Indah Permatasari', 'TRPL1A-009', 2, 8500000.00, 'Mandiri', 'Golongan 7', NULL, NULL, NULL, NULL, NULL),
 (10, 'Joko Prasetyo', 'TRPL1A-010', 2, 2500000.00, 'Bidikmisi', NULL, 'Suyanto', 'KIP-2024-00101', 700000.00, NULL, NULL),
 (11, 'Kartika Sari', 'TRPL1A-011', 4, 2500000.00, 'Bidikmisi', NULL, 'Sumarni', 'KIP-2024-00102', 700000.00, NULL, NULL),
-(12, 'Lukman Hakim', 'TRPL1A-012', 2, 2500000.00, 'Bidikmisi', NULL, 'Abdul Rohman', 'KIP-2024-00103', 750000.00, NULL, NULL),
-(13, 'Mega Wulandari', 'TRPL1A-013', 4, 2500000.00, 'Bidikmisi', NULL, 'Siti Aminah', 'KIP-2024-00104', 700000.00, NULL, NULL),
-(14, 'Naufal Rizky', 'TRPL1A-014', 6, 2500000.00, 'Bidikmisi', NULL, 'Hasan Basri', 'KIP-2024-00105', 800000.00, NULL, NULL),
-(15, 'Olivia Anggraeni', 'TRPL1A-015', 2, 2500000.00, 'Bidikmisi', NULL, 'Endang Suhartini', 'KIP-2024-00106', 700000.00, NULL, NULL),
-(16, 'Putra Aditya', 'TRPL1A-016', 4, 2500000.00, 'Bidikmisi', NULL, 'Bambang Sutrisno', 'KIP-2024-00107', 750000.00, NULL, NULL),
-(17, 'Qonitah Zahra', 'TRPL1A-017', 2, 2500000.00, 'Bidikmisi', NULL, 'Mahmud Haryono', 'KIP-2024-00108', 700000.00, NULL, NULL),
-(18, 'Rizal Fahmi', 'TRPL1A-018', 6, 2500000.00, 'Bidikmisi', NULL, 'Mulyadi', 'KIP-2024-00109', 800000.00, NULL, NULL),
-(19, 'Sinta Maharani', 'TRPL1A-019', 4, 0.00, 'Prestasi', NULL, NULL, NULL, NULL, 'Yayasan Djarum', 3.50),
-(20, 'Taufik Hidayat', 'TRPL1A-020', 2, 0.00, 'Prestasi', NULL, NULL, NULL, NULL, 'LPDP Kemenkeu', 3.25),
-(21, 'Ulfa Ramadhani', 'TRPL1A-021', 4, 0.00, 'Prestasi', NULL, NULL, NULL, NULL, 'Tanoto Foundation', 3.40),
-(22, 'Vino Bastian', 'TRPL1A-022', 6, 0.00, 'Prestasi', NULL, NULL, NULL, NULL, 'Beasiswa Bank Indonesia', 3.30),
-(23, 'Winda Kusuma', 'TRPL1A-023', 2, 0.00, 'Prestasi', NULL, NULL, NULL, NULL, 'Beasiswa Unggulan Kemdikbud', 3.50),
-(24, 'Xavier Pratama', 'TRPL1A-024', 4, 0.00, 'Prestasi', NULL, NULL, NULL, NULL, 'Beasiswa Pertamina', 3.25),
-(25, 'Yuliana Safitri', 'TRPL1A-025', 2, 0.00, 'Prestasi', NULL, NULL, NULL, NULL, 'Beasiswa Telkom Indonesia', 3.40),
-(26, 'Zainul Abidin', 'TRPL1A-026', 6, 0.00, 'Prestasi', NULL, NULL, NULL, NULL, 'Beasiswa Toyota Astra', 3.30),
-(27, 'Ahmad Fauzan', 'TRPL1A-027', 4, 0.00, 'Prestasi', NULL, NULL, NULL, NULL, 'Beasiswa Sampoerna Foundation', 3.50);
+(12, 'Lukman Hakim', 'TRPL1A-012', 2, 2500000.00, 'Bidikmisi', NULL, 'Abdul Rohman', 'KIP-2024-00103', 750000.00, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -94,7 +79,7 @@ ALTER TABLE `tabel_mahasiswa`
 -- AUTO_INCREMENT for table `tabel_mahasiswa`
 --
 ALTER TABLE `tabel_mahasiswa`
-  MODIFY `id_mahasiswa` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id_mahasiswa` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
